542d83e92dda5224fa4c600151ad781d068949a7
delphython <delphython@gmail.com>
2021-12-21 17:00:44 +0300

    Move data from template to view

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,35 +1,49 @@
 from django.shortcuts import render
 
+from .models import Place
+
 
 def index(request):
-    places = {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.62, 55.793676]
-          },
-          "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "{% static "/places/moscow_legends.json" %}"
-          }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "{% static "/places/roofs24.json" %}"
-          }
-        }
-      ]
+    features = []
+    geo_json_template = {
+        "type": "FeatureCollection",
+        "features": [],
     }
-    print("Hello!")
-    return render(request, "index.html", context=places)
+
+    # places = Place.objects.all()
+
+    places = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676],
+                },
+                "properties": {
+                    "title": "Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": "{% static '/places/moscow_legends.json' %}",
+                },
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676],
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "{% static '/places/roofs24.json' %}",
+                },
+            },
+        ],
+    }
+
+    context = {
+        "places": places,
+    }
+
+    return render(request, "index.html", context=context)
