f725602cbf0bd514957bf8b8ed87486de1c35f5c
delphython <delphython@gmail.com>
2021-12-23 07:29:21 +0300

    Add image adding in Place form in admin

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ("title",)
+    inlines = [
+        ImageInline,
+    ]
 
 
 @admin.register(Image)
