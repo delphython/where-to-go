85905addfa43380aa2a6db1ec966ff62722eb3b0
delphython <32509244+delphython@users.noreply.github.com>
2021-12-24 17:23:52 +0300

    Add delphython.pythonanywhere.com to ALLOWED_HOSTS

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "delphython.pythonanywhere.com",
+]
 
 
 # Application definition
