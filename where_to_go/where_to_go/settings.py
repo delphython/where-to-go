0ec06a22996f0e86f659f7fc21b4dc7780c272c4
delphython <32509244+delphython@users.noreply.github.com>
2021-12-24 17:06:55 +0300

    Set SECRET_KEY

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = (
+    "django-insecure-v4%z%!u6(*w8=kyp#m0clk)xa63bh5t2x5bwpkv+hf=hcko2b&"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
