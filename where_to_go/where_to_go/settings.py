0039c546a0c7e63985658aefb8a3123bc5cc7380
delphython <32509244+delphython@users.noreply.github.com>
2021-12-24 17:14:48 +0300

    Change STATIC_ROOT to STATIC_URL

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_URL = "/static/"
 # STATIC_URL = "/static/"
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
