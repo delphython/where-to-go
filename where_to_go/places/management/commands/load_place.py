b78ed0ae80d0c37037f58f96462d1d0945262f63
delphython <delphython@gmail.com>
2021-12-25 16:37:49 +0300

    Add load_place command

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -1,27 +1,57 @@
+import os
+from urllib.parse import urlparse, unquote
+
 import requests
 
 from django.core.management.base import BaseCommand
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
 
 from places.models import Place, Image
+
+
+def get_file_name(url):
+    parsed_url = urlparse(url)
+    path = unquote(parsed_url.path)
+    _, file_name = os.path.split(path)
+
+    return file_name
+
+
+def get_image_file(image_url):
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    img_temp = NamedTemporaryFile()
+    img_temp.write(response.content)
+    img_temp.flush()
+
+    return File(img_temp)
 
 
 def get_place_json_properties(json_url):
     response = requests.get(json_url)
     response.raise_for_status()
 
-    print(response.json())
-
     return response.json()
 
 
-def insert_place_to_db(place):
-    Place.objects.get_or_create(
+def add_place_to_db(place):
+    place_db = Place(
         title=place["title"],
         description_short=place["description_short"],
         description_Long=place["description_long"],
-        coordinates_lng=place["coordinates"][0],
-        coordinates_lat=place["coordinates"][0],
+        coordinates_lng=place["coordinates"]["lng"],
+        coordinates_lat=place["coordinates"]["lat"],
     )
+    place_db.save()
+
+    for image_url in place["imgs"]:
+        image = Image()
+        image.place = place_db
+        image.image_file.save(
+            get_file_name(image_url), get_image_file(image_url), save=True
+        )
 
 
 class Command(BaseCommand):
@@ -29,4 +59,4 @@ class Command(BaseCommand):
         parser.add_argument("json_file", type=str)
 
     def handle(self, *args, **options):
-        insert_place_to_db(get_place_json_properties(options["json_file"]))
+        add_place_to_db(get_place_json_properties(options["json_file"]))
