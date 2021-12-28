edd1bc36d4caa1dd28014c20dcb0b05e86345a82
delphython <32509244+delphython@users.noreply.github.com>
2021-12-28 09:43:26 +0300

    Change CharField to TextField for description_short

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -6,7 +6,7 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField("Заголовок", max_length=200)
-    description_short = models.CharField("Краткое описание", max_length=255)
+    description_short = models.TextField("Краткое описание")
     description_Long = HTMLField("Полное описание")
     coordinates_lng = models.CharField("Координаты долгота", max_length=17)
     coordinates_lat = models.CharField("Координаты широта", max_length=17)
